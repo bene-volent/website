63521e3a4d99c1919334f4fe2710f1760064367f
Bene-volent <raghavwastaken1@gmail.com>
2022-08-14 18:46:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const express = require('express')
+const {readFile} = require("fs")
+
+const app = express()
+app.use(express.static('./public'))
+
+const port = process.env.PORT||3000
+
+app.get('/', async (req, res) => {
+    readFile('./public/html/home.html','utf-8',(err,html)=>{
+        if (err) {
+            res.status(500).send("Server Error")
+            console.log(err,html);
+            return
+        }
+        res.contentType("html").send(html)
+    
+    })
+    
+})
+
+app.listen(port, () => console.log(`Port running on https://localhost:${port}`))
